c22773a33cc6caae0830f6b10e1645a0bd98e09b
Hemnixx <123105111@nitkkr.ac.in>
2025-12-26 22:06:43 +0530

    exception handling and logging

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,11 @@
+import sys
+from networksecurity.logging.logger import logger
+class NetworkSecurityError(Exception):
+    """Base class for exceptions in this module."""
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+            self.file_name,self.linenp,str(self.error_message))
