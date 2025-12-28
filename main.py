b260091e13c4aa64d446b899b8246d54232db43a
Hemnixx <123105111@nitkkr.ac.in>
2025-12-28 16:14:09 +0530

    Data validation complete

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation   
+
 from networksecurity.entity.config_entity import (
     DataIngestionConfig,
-    TrainingPipelineConfig
+    TrainingPipelineConfig,
+    DataValidationConfig
 )
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logger
+
 
 if __name__ == "__main__":
     try:
@@ -25,5 +29,23 @@ if __name__ == "__main__":
         logger.info("Data Ingestion Completed")
         print(dataingestionartifact)
 
+        # ---- Data Validation ----
+        data_validation_config = DataValidationConfig(
+            trainingpipelineconfig
+        )
+        data_validation = DataValidation(
+            dataingestionartifact,
+            data_validation_config
+        )
+
+        logger.info("Data Validation Started")
+
+        data_validation_artifact = (
+            data_validation.initiate_data_validation()
+        )
+
+        logger.info("Data Validation Completed")
+        print(data_validation_artifact)
+        
     except Exception as e:
         raise NetworkSecurityException(e)
